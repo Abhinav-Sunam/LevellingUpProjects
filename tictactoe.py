5f17a9eb9c37b4af0c917d196d8f63bd6f9fa5fb
Abhinav Sunam <sunamabhinav@gmail.com>
2024-11-22 21:48:45 +0530

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,15 +3,15 @@ import random
 def play():
     user=input("'r' for rock, 'p' for paper, 's' for scissors: \n")
     computer=random.choice(['r','p','s'])
+    print("") #prints an extra line just to make it look good
     if user==computer:
         return 'tie' #we come out of the def after we reach here
     if is_win(user,computer):
-         return "You Won!!!" #we come out of the def after we reach here
-    return 'You Lost :(' #the only way to reach here is if we lose, i.e. none of the above conditions are true 
+         return "YOU WON!!!" #we come out of the def after we reach here
+    return 'You Lost' #the only way to reach here is if we lose, i.e. none of the above conditions are true 
     
 def is_win(player,opponent):
         if(player=='r' and opponent == 's') or (player=='s' and opponent=='p') \
         or (player=='p'and opponent=='r'):
-             return True
-        
+             return True   
 print(play())        
